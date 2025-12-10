689d6dde06071c4fd0ae0777e82ef5d562cc83af
Carlos Habekost <carlos.habekost@gmail.com>
2025-12-10 12:11:04 -0300

    Ajustes: header mobile, timeline faixas pretas, footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,7 @@ export default {
       {
         light: {
           // eslint-disable-next-line @typescript-eslint/no-require-imports, no-undef
-          ...require("daisyui/src/theming/themes")["light"],
+          ...require("daisyui/src/theming/themes")["luxury"],
           primary: "#162f4f",
           "primary-content": "white",
           secondary: "#ffc718",
